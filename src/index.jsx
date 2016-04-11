35ce53ab21e06473bacc0359b30150038a4641c1
jean <jean.ma.1986@gmail.com>
2016-04-11 11:21:13 +0800

    fixed

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 require("bower/normalize-css/normalize.css");
 import Hello from "./components/hello.jsx";
 
-var view = document.getElementById("view");
+let view = document.getElementById("view");
 
 ReactDom.render(
     <Hello></Hello>
--- webpack.config.js
+++ webpack.config.js
@@ -1,82 +1,83 @@
 var webpack = require("webpack");
-var path=require("path");
+var path = require("path");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var CleanWebpackPlugin = require('clean-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: {
-        index:"./src/index.jsx",
-        vendor:["react","react-dom"]
-    },
-    output: {
-        path: path.join(__dirname,"dist"),
-        filename: "bundle.js"
-        //publicPath:path.join(__dirname,"dist")
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.css$/,
-                //loader: "style/url!file"
-                loader:ExtractTextPlugin.extract("style-loader", "css-loader")
-            },
-            {
-                test: /\.jsx?$/, 
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel', // 'babel-loader' is also a legal name to reference
-                query: {
-                    presets: ['es2015', 'react']
-                }
-            }
-        ],
-        preLoaders:[{
-            test://
-        }]
-    },
-    resolve:{
-        //设置别名
-        alias:{
-            bower:path.join(__dirname,"bower_components")
-        }
-    },
-    plugins: [
-        new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.bundle.js"),
-        //全局module
-        new webpack.ProvidePlugin({
-            React: 'react',
-            ReactDom: "react-dom"
-        }),
-        //把所有的css打包到style.css
-        new ExtractTextPlugin("style.css",{
-            allChunks:true
-        }),
-        //清除发布目录
-        new CleanWebpackPlugin(['dist'], {
-            root: __dirname,
-            verbose: true,
-            dry: false
-        }),
-        //html
-        new HtmlWebpackPlugin({
-            filename:"index.html",
-            template: './src/index.html',
-            hash:true,
-            inject:false,
-            files:{
-                css:["style.css"],
-                js:["vendor.bundle.js","bundle.js"],
-                // chunks:{
-                //     vendor:{
-                //         entry:"dist/vendor.bundle.js",
-                //         css:[]
-                //     },
-                //     index:{
-                //         entry:"dist/bundle.js",
-                //         css:["dist/style.css"]
-                //     }
-                // }
-            }
-        })
-    ]
+	entry: {
+		index: "./src/index.jsx",
+		vendor: ["react", "react-dom"]
+	},
+	output: {
+		path: path.join(__dirname, "dist"),
+		filename: "bundle.js"
+		//publicPath:path.join(__dirname,"dist")
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.css$/,
+				loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+			},
+			{
+				test: /\.jsx?$/,
+				exclude: /(node_modules|bower_components)/
+				// 'babel-loader' is also a legal name to reference
+				//loaders: ["babel-loader?presets[]=es2016,presets[]=react", "eslint-loader?{rules:{semi:0}}"]
+				,loader:"babel-loader?presets[]=es2015,presets[]=react"
+			}
+		]
+		,preLoaders: [
+			{
+				test: /\.jsx$/
+				, loader: "eslint-loader"
+				, exclude: /node_modules/}
+		]
+	},
+	resolve: {
+		//设置别名
+		alias: {
+			bower: path.join(__dirname, "bower_components")
+		}
+	},
+	plugins: [
+		new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.bundle.js"),
+		//全局module
+		new webpack.ProvidePlugin({
+			React: 'react',
+			ReactDom: "react-dom"
+		}),
+		//把所有的css打包到style.css
+		new ExtractTextPlugin("style.css", {
+			allChunks: true
+		}),
+		//清除发布目录
+		new CleanWebpackPlugin(['dist'], {
+			root: __dirname,
+			verbose: true,
+			dry: false
+		}),
+		//html
+		new HtmlWebpackPlugin({
+			filename: "index.html",
+			template: './src/index.html',
+			hash: true,
+			inject: false,
+			files: {
+				css: ["style.css"],
+				js: ["vendor.bundle.js", "bundle.js"],
+				// chunks:{
+				//     vendor:{
+				//         entry:"dist/vendor.bundle.js",
+				//         css:[]
+				//     },
+				//     index:{
+				//         entry:"dist/bundle.js",
+				//         css:["dist/style.css"]
+				//     }
+				// }
+			}
+		})
+	]
 };
