ce79f60b1f5e46061e523ad0006e017b3c42b44a
jean <jean.ma.1986@gmail.com>
2016-04-15 17:41:55 +0800

    implement animation

--- src/pages/animation.jsx
+++ src/pages/animation.jsx
@@ -1,15 +1,38 @@
 require("assets/sass/animation-example.sass");
 import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 
-class Animation extends React.Component{
-	render(){
+class Animation extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			items: ['hello', 'world', 'click', 'me']
+		};
+	}
+
+	handleRemove(i) {
+		let newItems = this.state.items.slice();
+		newItems.splice(i, 1);
+		this.setState({items: newItems});
+	}
+
+	render() {
+		let items = this.state.items.map((item, i)=>
+			<div key={item} onClick={this.handleRemove.bind(this, i)}>
+				{item}
+			</div>
+		);
 		return (
 			<div>
-				<p><button>toggle</button></p>
-				<ReactCSSTransitionGroup transitionName="example"
-										 transitionEnterTimeout={2000}
-										 transitionLeaveTimeout={2000}>
-					<p key="1">I am animation!</p>
+				<button onClick={
+				()=>{
+					let newItems =this.state.items.concat([prompt('Enter some text')]);
+					this.setState({items: newItems});
+				}
+				}>Add Item
+				</button>
+				<ReactCSSTransitionGroup transitionName="example" transitionEnterTimeout={500}
+										 transitionLeaveTimeout={300}>
+					{items}
 				</ReactCSSTransitionGroup>
 			</div>
 		);
