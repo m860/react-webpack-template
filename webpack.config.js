bc8868d07d471152d7084b4a95ca9ca0a57c8d9f
jean <jean.ma.1986@gmail.com>
2017-02-23 16:18:30 +0800

    done webpack2 confiruration

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,52 @@ var webpack = require("webpack");
 var CleanWebpackPlugin = require('clean-webpack-plugin');
 var autoprefixer = require('autoprefixer');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+var isProduction = function () {
+	return process.env['NODE_ENV'] === 'production';
+};
+
+var plugins=[
+	new webpack.ProvidePlugin({
+		React: 'react'
+		, ReactDOM: "react-dom"
+		, classNames: "classNames"
+	}),
+	new HtmlWebpackPlugin({
+		filename: "index.html",
+		template: './src/index.html',
+		inject: false
+	}),
+	new webpack.LoaderOptionsPlugin({
+		options: {
+			postcss: [
+				autoprefixer({
+					browsers: ['> 5%']
+				})
+			]
+		}
+	}),
+	new ExtractTextPlugin(isProduction()?"[contenthash].css":"style.css"),
+	new webpack.optimize.CommonsChunkPlugin({
+		name: "vendor"
+		, filename: "vendor.bundle.js"
+	}),
+	new CleanWebpackPlugin(['dist'], {
+		root: __dirname,
+		verbose: true,
+		dry: false
+	})
+];
+
+if(isProduction()){
+	plugins.push(new UglifyJSPlugin());
+	plugins.push(new webpack.DefinePlugin({
+		'process.env': {
+			NODE_ENV: JSON.stringify('production')
+		}
+	}))
+}
 
 module.exports = {
 	entry: {
@@ -18,8 +64,8 @@ module.exports = {
 	},
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js',
-		chunkFilename: "[chunkhash].bundle.js"
+		filename: isProduction() ? 'bundle.min.js' : 'bundle.js',
+		chunkFilename: isProduction() ? "[chunkhash].min.js" : "[id].js"
 	},
 	module: {
 		rules: [
@@ -53,38 +99,21 @@ module.exports = {
 			}, {
 				test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
 				loader: "url-loader?limit=10000&mimetype=application/font-woff"
+			},{
+				test: /\.(jpe?g|png|gif|svg)$/i,
+				loaders: [
+					'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+					'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
+				]
+			},{
+				test: /\.js$/,
+				loader: "eslint-loader",
+				exclude: [
+					path.resolve(__dirname,"node_modules")
+				],
+				enforce:"pre"
 			}
 		]
 	},
-	plugins: [
-		new webpack.ProvidePlugin({
-			React: 'react'
-			, ReactDOM: "react-dom"
-			, classNames: "classNames"
-		}),
-		new HtmlWebpackPlugin({
-			filename: "index.html",
-			template: './src/index.html',
-			inject: false
-		}),
-		new webpack.LoaderOptionsPlugin({
-			options: {
-				postcss: [
-					autoprefixer({
-						browsers: ['> 5%']
-					})
-				]
-			}
-		}),
-		new ExtractTextPlugin("style.css"),
-		new webpack.optimize.CommonsChunkPlugin({
-			name: "vendor"
-			, filename: "vendor.bundle.js"
-		}),
-		new CleanWebpackPlugin(['dist'], {
-			root: __dirname,
-			verbose: true,
-			dry: false
-		})
-	]
+	plugins: plugins
 }
